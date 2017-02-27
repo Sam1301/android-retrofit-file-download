7fb3ff64ce19ab73350a0be154913bc502336522
Sam <saumya.bhatnagar.sb@gmail.com>
2017-02-27 17:44:09 +0530

    responsebody update

---
+++ app/src/main/java/com/learn2crack/filedownload/DownloadService.java
@@ -0,0 +1,218 @@
+package com.learn2crack.filedownload;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.Toast;
+
+import com.learn2crack.filedownload.models.Download;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okio.Buffer;
+import okio.BufferedSource;
+import okio.ForwardingSource;
+import okio.Okio;
+import okio.Source;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
+
+public class DownloadService extends IntentService {
+
+    public DownloadService() {
+        super("Download Service");
+    }
+
+    private NotificationCompat.Builder notificationBuilder;
+    private NotificationManager notificationManager;
+    private int totalFileSize;
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.stat_sys_upload)
+                .setContentTitle("Download")
+                .setContentText("Downloading File")
+                .setAutoCancel(true);
+        notificationManager.notify(0, notificationBuilder.build());
+
+        initDownload();
+
+    }
+    int timeCount = 1;
+    private void initDownload(){
+        final long startTime = System.currentTimeMillis();
+        timeCount = 1;
+        final ProgressListener progressListener = new ProgressListener() {
+            @Override public void update(long bytesRead, long contentLength, boolean done) {
+                // update ui after every 3 second
+                if (System.currentTimeMillis() - startTime > 1000 * timeCount) {
+                    Download download = new Download();
+                    double per = ((double) bytesRead) / contentLength;
+                    download.setProgress((int) (per * 100));
+                    download.setCurrentFileSize((int) (bytesRead / (Math.pow(1024, 2))));
+                    download.setTotalFileSize((int) (contentLength / (Math.pow(1024, 2))));
+                    sendNotification(download);
+                    timeCount++;
+                }
+            }
+        };
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new Interceptor() {
+                    @Override public Response intercept(Chain chain) throws IOException {
+                        Response originalResponse = chain.proceed(chain.request());
+                        return originalResponse.newBuilder()
+                                .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                                .build();
+                    }
+                })
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://download.learn2crack.com/")
+                .client(client)
+                .build();
+
+        RetrofitInterface retrofitInterface = retrofit.create(RetrofitInterface.class);
+
+        Call<ResponseBody> request = retrofitInterface.downloadFile();
+        try {
+
+            downloadFile(request.execute().body());
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private void downloadFile(ResponseBody body) throws IOException {
+
+        int count;
+        byte data[] = new byte[1024 * 4];
+        long fileSize = body.contentLength();
+        InputStream bis = new BufferedInputStream(body.byteStream(), 1024 * 8);
+        long total = 0;
+        long startTime = System.currentTimeMillis();
+        int timeCount = 1;
+        while ((count = bis.read(data)) != -1) {
+
+            total += count;
+            totalFileSize = (int) (fileSize / (Math.pow(1024, 2)));
+            double current = Math.round(total / (Math.pow(1024, 2)));
+
+            int progress = (int) ((total * 100) / fileSize);
+
+            long currentTime = System.currentTimeMillis() - startTime;
+
+            Download download = new Download();
+            download.setTotalFileSize(totalFileSize);
+
+            if (currentTime > 1000 * timeCount) {
+
+                download.setCurrentFileSize((int) current);
+                download.setProgress(progress);
+//                sendNotification(download);
+                timeCount++;
+            }
+        }
+        onDownloadComplete();
+        bis.close();
+    }
+
+    private void sendNotification(Download download){
+
+        sendIntent(download);
+        notificationBuilder.setProgress(100,download.getProgress(),false);
+        notificationBuilder.setContentText(String.format("Downloaded (%d/%d) MB",download.getCurrentFileSize(),download.getTotalFileSize()));
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+
+    private void sendIntent(Download download){
+
+        Intent intent = new Intent(MainActivity.MESSAGE_PROGRESS);
+        intent.putExtra("download",download);
+        LocalBroadcastManager.getInstance(DownloadService.this).sendBroadcast(intent);
+    }
+
+    private void onDownloadComplete(){
+
+        Download download = new Download();
+        download.setProgress(100);
+        sendIntent(download);
+
+        notificationManager.cancel(0);
+        notificationBuilder.setProgress(0,0,false);
+        notificationBuilder.setContentText("File Downloaded");
+        notificationManager.notify(0, notificationBuilder.build());
+
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        notificationManager.cancel(0);
+    }
+
+    private static class ProgressResponseBody extends ResponseBody {
+
+        private final ResponseBody responseBody;
+        private final ProgressListener progressListener;
+        private BufferedSource bufferedSource;
+
+        public ProgressResponseBody(ResponseBody responseBody, ProgressListener progressListener) {
+            this.responseBody = responseBody;
+            this.progressListener = progressListener;
+        }
+
+        @Override public MediaType contentType() {
+            return responseBody.contentType();
+        }
+
+        @Override public long contentLength() {
+            return responseBody.contentLength();
+        }
+
+        @Override public BufferedSource source() {
+            if (bufferedSource == null) {
+                bufferedSource = Okio.buffer(source(responseBody.source()));
+            }
+            return bufferedSource;
+        }
+
+        private Source source(Source source) {
+            return new ForwardingSource(source) {
+                long totalBytesRead = 0L;
+
+                @Override public long read(Buffer sink, long byteCount) throws IOException {
+                    long bytesRead = super.read(sink, byteCount);
+                    // read() returns the number of bytes read, or -1 if this source is exhausted.
+                    totalBytesRead += bytesRead != -1 ? bytesRead : 0;
+                    progressListener.update(totalBytesRead, responseBody.contentLength(), bytesRead == -1);
+                    return bytesRead;
+                }
+            };
+        }
+    }
+
+    interface ProgressListener {
+        void update(long bytesRead, long contentLength, boolean done);
+    }
+}
